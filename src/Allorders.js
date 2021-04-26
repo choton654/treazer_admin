24a927fa80fd5349ddb7d3c02d7068df3c204d96
choton654 <choton654@gmail.com>
2021-04-26 22:00:11 +0530

    ..

--- src/Allorders.js
+++ src/Allorders.js
@@ -1,23 +1,30 @@
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+const { height } = Dimensions.get("window");
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { Button, ActivityIndicator } from "react-native-paper";
 import axios from "axios";
 import BASE_URL from "./api";
 import { orderContext } from "./orderContext";
 
 const Allorders = () => {
-  const { state: orderState, dispatch: orderDispatch } = orderContext();
+  const { state: odrState, dispatch: orderDispatch } = orderContext();
   useEffect(() => {
     getAllOrders();
   }, []);
 
+  const [acceptOrderReq, setacceptOrderReq] = useState(true);
+
   const getAllOrders = () => {
-    if (orderState.orders === null) {
+    if (odrState.orders === null) {
+      setacceptOrderReq(false);
       axios
         .get(`${BASE_URL}/api/order/adminallorder`)
         .then((res) => {
           const { order } = res.data;
           console.log(order);
           orderDispatch({ type: "GET_ALL_ORDERS", payload: order });
+          setacceptOrderReq(true);
         })
         .catch((err) => {
           console.log(err);
@@ -26,9 +33,237 @@ const Allorders = () => {
   };
 
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <ScrollView
+      contentContainerStyle={{
+        width: "100%",
+        backgroundColor: "#ffffff",
+      }}>
+      {odrState && odrState.orders ? (
+        odrState.orders.length > 0 ? (
+          odrState.orders.map((order, idx) => (
+            <View
+              key={idx}
+              style={{
+                height: 220,
+                paddingHorizontal: 10,
+                marginTop: 10,
+              }}>
+              <View
+                style={{
+                  borderColor: "#455a64",
+                  borderBottomLeftRadius: 20,
+                  borderBottomRightRadius: 20,
+                  borderTopRightRadius: 20,
+                  borderTopLeftRadius: 20,
+                  borderLeftWidth: 1,
+                  borderRightWidth: 1,
+                  borderTopWidth: 1,
+                  borderBottomWidth: 1,
+                  borderStyle: "solid",
+                }}>
+                <Text
+                  style={{
+                    marginTop: 5,
+                    marginLeft: 10,
+                    fontWeight: "600",
+                    fontSize: 15,
+                    color: "#424242",
+                    letterSpacing: 1,
+                  }}>
+                  {order.userId && order.userId.username}
+                </Text>
+                <Text
+                  style={{
+                    marginVertical: 5,
+                    marginLeft: 10,
+                    fontWeight: "400",
+                    fontSize: 12,
+                    letterSpacing: 1,
+                    color: "#424242",
+                  }}>
+                  Order Time:
+                  <Text
+                    style={{
+                      marginHorizontal: 5,
+                      fontWeight: "700",
+                      fontSize: 12,
+                      color: "#424242",
+                      letterSpacing: 1,
+                    }}>
+                    {order.createdAt}
+                  </Text>
+                </Text>
+                <View
+                  style={{
+                    height: 50,
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    borderTopWidth: 1,
+                    borderTopColor: "#455a64",
+                    borderStyle: "solid",
+                    borderBottomWidth: 1,
+                    borderBottomColor: "#455a64",
+                    marginTop: 10,
+                  }}>
+                  <View
+                    style={{
+                      width: "50%",
+                      borderRightColor: "#455a64",
+                      borderRightWidth: 1,
+                      borderStyle: "solid",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      // padding: 5,
+                    }}>
+                    <Text
+                      style={{
+                        fontWeight: "600",
+                        fontSize: 15,
+                        color: "#9e9e9e",
+                        letterSpacing: 1,
+                      }}>
+                      Order Amount
+                    </Text>
+                    <Text
+                      style={{
+                        marginBottom: 5,
+                        fontWeight: "800",
+                        fontSize: 15,
+                        color: "#424242",
+                        letterSpacing: 1,
+                      }}>
+                      Rs.{order.totalPrice}
+                    </Text>
+                  </View>
+                  <View
+                    style={{
+                      width: "50%",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}>
+                    <Text
+                      style={{
+                        fontWeight: "600",
+                        fontSize: 15,
+                        color: "#9e9e9e",
+                        letterSpacing: 1,
+                      }}>
+                      Payment Type
+                    </Text>
+                    <Text
+                      style={{
+                        marginBottom: 5,
+                        fontWeight: "800",
+                        fontSize: 15,
+                        color: "#424242",
+                        letterSpacing: 1,
+                      }}>
+                      Pre-paid
+                    </Text>
+                  </View>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    marginVertical: 10,
+                  }}>
+                  <Ionicons
+                    name='location-sharp'
+                    size={20}
+                    color='#4fc3f7'
+                    style={{ marginLeft: 10 }}
+                  />
+                  <Text
+                    style={{
+                      marginHorizontal: 5,
+                      fontWeight: "600",
+                      fontSize: 12,
+                      color: "#424242",
+                      letterSpacing: 1,
+                    }}>
+                    {order.shippingaddress &&
+                      order.shippingaddress.formattedAddress}
+                  </Text>
+                </View>
+                {acceptOrderReq ? (
+                  <View
+                    style={{
+                      justifyContent: "center",
+                      alignItems: "center",
+                      marginBottom: 5,
+                    }}>
+                    <Button
+                      mode='contained'
+                      // onPress={() => acceptOrder(user._id, order._id)}
+                      style={{
+                        marginBottom: 10,
+                        width: "20%",
+                        height: 30,
+                        backgroundColor: "#4fc3f7",
+                        boxShadow: "0px 2px 5px 2px #bdbdbd",
+                      }}
+                      labelStyle={{
+                        color: "#ffffff",
+                        fontWeight: "700",
+                        fontSize: 12,
+                        marginHorizontal: "none",
+                      }}>
+                      Done
+                    </Button>
+                  </View>
+                ) : (
+                  <View
+                    style={{
+                      marginHorizontal: "auto",
+                      backgroundColor: "#ffffff",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      width: "60%",
+                      height: 40,
+                    }}>
+                    <ActivityIndicator
+                      animating={true}
+                      color='#82b1ff'
+                      size='small'
+                      style={{ marginVertical: 10 }}
+                    />
+                  </View>
+                )}
+              </View>
+            </View>
+          ))
+        ) : (
+          <View
+            style={{
+              height: height * 0.7,
+              backgroundColor: "#ffffff",
+              justifyContent: "center",
+              padding: 20,
+            }}>
+            <Text
+              style={{
+                textAlign: "Center",
+                fontSize: 20,
+                letterSpacing: 3,
+                fontWeight: "700",
+                fontFamily: "Open Sans",
+                color: "#bdbdbd",
+              }}>
+              There is no new orders for you right now
+            </Text>
+          </View>
+        )
+      ) : (
+        <View
+          style={{
+            height: height * 0.7,
+            backgroundColor: "#ffffff",
+            justifyContent: "center",
+          }}>
+          <ActivityIndicator animating={true} color='#82b1ff' size='large' />
+        </View>
+      )}
+    </ScrollView>
   );
 };
 
--- src/rootnavigation.js
+++ src/rootnavigation.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, Platform, Dimensions } from "react-native";
+const MORE_ICON = Platform.OS === "ios" ? "dots-horizontal" : "dots-vertical";
+import { Appbar } from "react-native-paper";
 import Allorders from "./Allorders";
+const { height } = Dimensions.get("window");
 const Rootnavigation = () => {
   return (
     <View>
-      <Text>from rootnavigation</Text>
-      <View>
+      <Appbar.Header style={{ top: 0 }}>
+        <Appbar.Content title='Admin' subtitle={"Pannel"} />
+        <Appbar.Action icon='magnify' onPress={() => {}} />
+        <Appbar.Action icon={MORE_ICON} onPress={() => {}} />
+      </Appbar.Header>
+      <View style={{ height: height * 0.9 }}>
         <Allorders />
       </View>
     </View>
