4c52d4bfa1228076b341c711731200ca4fecd888
choton654 <choton654@gmail.com>
2021-04-26 17:48:44 +0530

    ..

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Rootnavigation from "./src/rootnavigation";
+import * as PusherPushNotifications from "@pusher/push-notifications-web";
 
+const beamsClient = new PusherPushNotifications.Client({
+  instanceId: "4c134700-4141-489d-b536-a6417609ba38",
+});
 export default function App() {
+  beamsClient
+    .start()
+    .then((beamsClient2) => beamsClient2.getDeviceId())
+    .then((deviceId) =>
+      console.log("Successfully registered with Beams. Device ID:", deviceId)
+    )
+    .then(() => beamsClient.addDeviceInterest("order"))
+    .then(() => beamsClient.getDeviceInterests())
+    .then((interests) => console.log("Current interests:", interests))
+    .catch((err) => {
+      console.log(err);
+    });
   return (
     <View style={styles.container}>
+      <Rootnavigation />
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -14,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/rootnavigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Rootnavigation = () => {
+  return (
+    <View>
+      <Text>from rootnavigation</Text>
+    </View>
+  );
+};
+
+export default Rootnavigation;
+
+const styles = StyleSheet.create({});
