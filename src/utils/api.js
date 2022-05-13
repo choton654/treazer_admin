ee7e8354f5c6dfe749dd3c6be8d5951003302b16
choton654 <choton654@gmail.com>
2022-05-13 18:06:15 +0530

    product delete completed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
 // const BASE_URL = `https://groovy-gearbox-322013.el.r.appspot.com`;
-// const BASE_URL = "https://treazer.tk"
-const BASE_URL = `http://localhost:7000`;
+const BASE_URL = "https://treazer.tk"
+// const BASE_URL = `http://localhost:7000`;
 module.exports = BASE_URL;
